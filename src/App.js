e004d83a757ab82b80a9f3047ae300f51f5b594b
Yukari <80179287+koko2aru@users.noreply.github.com>
2023-06-05 15:04:12 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -46,30 +46,3 @@ const App = () => {
 };
 
 export default App;
-
-import React, { useContext } from 'react';
-import { AppContext } from '../context/AppContext';
-
-const Currency = () => {
-const { dispatch, currency } = useContext(AppContext);
-    const changeCurrency = (val) => {
-        dispatch({
-                type: 'CHG_CURRENCY',
-                payload: val,
-            })
-    };
-    return (
-        <div className='alert alert-secondary'>
-      <select class="selectpicker" title='Currency (£ Pound)' data-style="btn-info" onChange={event=>changeCurrency(event.target.value)}>
-        
-        <option value="$">$ Dollar</option>
-        <option value="£" default>£ Pound</option>
-        <option value="€">€ Euro</option>
-        <option value="₹">₹ Ruppee</option>
-      </select>	
-    </div>
-    );
-    
-};
-
-export default Currency;
